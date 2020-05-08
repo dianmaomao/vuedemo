d1e5b9fdb0016a2a88399e2aafaddeb560fc91b0
25198 <WENBIN.QIU@HAND-CHINA.COM>
2020-05-08 21:15:50 +0800

    fix:自动化构建第6次

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="邱文斌大帅逼1111第五次"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="邱文斌大帅逼1111第五次" />
+    <p>{{data}}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      data: "前端自动化构建"
+    };
+  },
   components: {
     HelloWorld
   }
-}
+};
 </script>
 
 <style>
